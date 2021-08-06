3cedd5ea81652295512df7157053acbc6f10fd43
Vladimir Nikulin <mail.jorey@gmail.com>
2021-08-06 21:09:39 +0300

    Added MVVM menu. Now, user can change pages.

---
+++ BallScanner/MVVM/ViewModels/CalibrateVM.cs
@@ -0,0 +1,10 @@
+﻿namespace BallScanner.MVVM.ViewModels
+{
+    public class CalibrateVM
+    {
+        public CalibrateVM()
+        {
+
+        }
+    }
+}
---
+++ BallScanner/MVVM/ViewModels/MenuVM.cs
@@ -0,0 +1,50 @@
+﻿using BallScanner.MVVM.Core;
+
+namespace BallScanner.MVVM.ViewModels
+{
+    public class MenuVM : BaseViewModel
+    {
+        private object _currentActionVM;
+        public object CurrentActionVM
+        {
+            get { return _currentActionVM; }
+            set
+            {
+                _currentActionVM = value;
+                OnPropertyChanged("CurrentActionVM");
+            }
+        }
+
+        // Команда, реагирующая на нажатие кнопки из меню
+        public RelayCommand MenuButtonClick { get; set; }
+
+        private ScanVM scanVM;
+        private CalibrateVM calibrateVM;
+
+        public MenuVM()
+        {
+            scanVM = new ScanVM();
+            calibrateVM = new CalibrateVM();
+
+            CurrentActionVM = scanVM;
+
+            // Повесить команды на MenuButtonClick
+            MenuButtonClick = new RelayCommand(OnMenuButtonClick);
+        }
+
+        public void OnMenuButtonClick(object param)
+        {
+            System.Diagnostics.Debug.WriteLine($"Clicked: {param as string}");
+            string name = param as string;
+
+            if (name == "Scan")
+            {
+                CurrentActionVM = scanVM;
+            }
+            else if (name == "Calibrate")
+            {
+                CurrentActionVM = calibrateVM;
+            }
+        }
+    }
+}
