b8bf761019c8b3cb9111983df1451d51b72bbc3e
Vladimir Nikulin <mail.jorey@gmail.com>
2021-08-23 21:53:50 +0300

    Сделана страница "Настройки". Сделан кастомный элемент управления "joel:RadioButton". Добавлены векторные изображения флагов Америки и России. Добавлены иконки светлого и темного режимов. Написана логика переключения языка и темы.

---
+++ Joel.Controls/RadioButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Joel.Controls
+{
+    public class RadioButton : System.Windows.Controls.RadioButton
+    {
+        // Icon
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(UIElement), typeof(RadioButton), new PropertyMetadata(default(UIElement)));
+
+        public UIElement Icon
+        {
+            get { return (UIElement)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        static RadioButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RadioButton),
+                new FrameworkPropertyMetadata(typeof(RadioButton)));
+        }
+    }
+}
