3ffd604b4a58f64dfee4289c38c984ae5146a1ac
Vladimir Nikulin <mail.jorey@gmail.com>
2021-09-18 20:12:38 +0300

    Added table style (beta). Changed the style of the control button (pre-release).

---
+++ BallScanner/MVVM/Views/Test.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BallScanner.MVVM.Views
+{
+    class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public byte Age { get; set; }
+    }
+
+    public partial class Test : Window
+    {
+        public Test()
+        {
+            InitializeComponent();
+
+            Person[] persons = new Person[10];
+            for (int i = 0; i < persons.Length; i++)
+                persons[i] = new Person()
+                {
+                    Name = "Name",
+                    Surname = "Surname",
+                    Age = 10
+                };
+
+            TestDataGrid.ItemsSource = persons;
+        }
+    }
+}
