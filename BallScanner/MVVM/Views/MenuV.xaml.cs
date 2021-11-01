df5e1eefe30806c618bc41725d15a9670b38f400
Vladimir Nikulin <mail.jorey@gmail.com>
2021-11-01 21:22:44 +0300

    Добавлена кнопка переключения визуального состояния для пунктов меню.

--- BallScanner/MVVM/Views/MenuV.xaml.cs
+++ BallScanner/MVVM/Views/MenuV.xaml.cs
@@ -4,9 +4,24 @@ namespace BallScanner.MVVM.Views
 {
     public partial class MenuV : UserControl
     {
+        private bool isMinState = false;
+
         public MenuV()
         {
             InitializeComponent();
+        }
+
+        private void MyCollapseButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            isMinState = !isMinState;
+
+            if (isMinState)
+            {
+                MyLogo.Visibility = System.Windows.Visibility.Collapsed;
+            } else
+            {
+                MyLogo.Visibility = System.Windows.Visibility.Visible;
+            }
         }
     }
 }
---
+++ Joel.Controls/CollapseButton.cs
@@ -0,0 +1,92 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Joel.Controls
+{
+    public class CollapseButton : Button
+    {
+        // Icon
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(Geometry), typeof(CollapseButton), new PropertyMetadata(default(Geometry)));
+
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Icon Width & Height
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register(nameof(IconWidth), typeof(double), typeof(CollapseButton), new PropertyMetadata(default(double)));
+
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconHeightProperty =
+            DependencyProperty.Register(nameof(IconHeight), typeof(double), typeof(CollapseButton), new PropertyMetadata(default(double)));
+
+        public double IconHeight
+        {
+            get { return (double)GetValue(IconHeightProperty); }
+            set { SetValue(IconHeightProperty, value); }
+        }
+
+        // Icon Fill & Stroke
+        public static readonly DependencyProperty IconFillProperty =
+            DependencyProperty.Register(nameof(IconFill), typeof(Brush), typeof(CollapseButton), new PropertyMetadata(default(Brush)));
+
+        public Brush IconFill
+        {
+            get { return (Brush)GetValue(IconFillProperty); }
+            set { SetValue(IconFillProperty, value); }
+        }
+
+        //public static readonly DependencyProperty IconStrokeProperty =
+        //    DependencyProperty.Register(nameof(IconStroke), typeof(Brush), typeof(MenuButton), new PropertyMetadata(default(Brush)));
+
+        //public Brush IconStroke
+        //{
+        //    get { return (Brush)GetValue(IconStrokeProperty); }
+        //    set { SetValue(IconStrokeProperty, value); }
+        //}
+
+        // Icon position
+        public static readonly DependencyProperty IconPositionProperty =
+            DependencyProperty.Register(nameof(IconPosition), typeof(Dock), typeof(CollapseButton), new PropertyMetadata(default(Dock)));
+
+        public Dock IconPosition
+        {
+            get { return (Dock)GetValue(IconPositionProperty); }
+            set { SetValue(IconPositionProperty, value); }
+        }
+
+        // Text alingment
+        public static readonly DependencyProperty TextAlignmentProperty =
+            DependencyProperty.Register(nameof(TextAlignment), typeof(TextAlignment), typeof(CollapseButton), new PropertyMetadata(default(TextAlignment)));
+
+        public TextAlignment TextAlignment
+        {
+            get { return (TextAlignment)GetValue(TextAlignmentProperty); }
+            set { SetValue(TextAlignmentProperty, value); }
+        }
+
+        // Corner radius
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(CollapseButton), new PropertyMetadata(default(CornerRadius)));
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(TextAlignmentProperty); }
+            set { SetValue(TextAlignmentProperty, value); }
+        }
+
+        static CollapseButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CollapseButton), new FrameworkPropertyMetadata(typeof(CollapseButton)));
+        }
+    }
+}
